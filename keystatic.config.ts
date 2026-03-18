a33e2bde48b8f15c12d475494dade3f0c6c01957
isamu-takemoto <takeisa0201@gmail.com>
2026-03-18 11:31:09 +0900

    記事プレビュー機能実装

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -22,7 +22,16 @@ export default config({
         ? 'https://takeisa.dev/blog/{slug}'
         : '/blog/{slug}',
       schema: {
-        slug: fields.text({ label: 'スラグ（URL）' }),
+        slug: fields.text({
+          label: 'スラグ（URL）',
+          defaultValue: (() => {
+            const d = new Date();
+            const yyyy = d.getFullYear();
+            const mm = String(d.getMonth() + 1).padStart(2, '0');
+            const dd = String(d.getDate()).padStart(2, '0');
+            return `${yyyy}-${mm}-${dd}-1`;
+          })(),
+        }),
         title: fields.text({ label: 'タイトル' }),
         description: fields.text({ label: '説明', multiline: true }),
         pubDate: fields.date({ label: '公開日' }),
